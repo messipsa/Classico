0de4151b38e26a3337bda391e381a6495603b4d5
Messipsa <im_tamazouzt@esi.dz>
2022-03-06 16:13:42 +0100

    connection to mongoDB

--- server.js
+++ server.js
@@ -1,1 +1,50 @@
-console.log("salim tir lil");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const port = process.env.PORT;
+const url = process.env.URI;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+console.log(process.env.PORT);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log(`Mongoose default connection to ${url}`);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`Mongoose default connection error : ${err}`);
+});
+
+mongoose.connection.on("disconnect", () => {
+  console.log("Mongoose default connection disconnected");
+});
+
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log("Mongoose connection closed");
+    process.exit(0);
+  });
+});
